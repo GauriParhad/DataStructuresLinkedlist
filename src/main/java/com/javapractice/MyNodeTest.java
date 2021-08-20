c53bf73384fe28dda3be4aebbe22d8d42bdb7a22
Gaurip10 <thegauriparhad@gmail.com>
2021-08-20 22:08:36 +0530

    Added: Delecting last element

--- src/main/java/com/javapractice/MyNodeTest.java
+++ src/main/java/com/javapractice/MyNodeTest.java
@@ -35,3 +35,21 @@ public class myLinkedList {
         Assert.assertTrue(result);
     }
 }
+@Test
+public class myLinkedList {
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
+        MyNode<Integer> myFirstNode = new MyNODE<>(56);
+        MyNode<Integer> mySecondNode = new MyNODE<>(30);
+        MyNode<Integer> myThirdNode = new MyNODE<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.insert(myFirstNode,mySecondNode);
+        myLinkedList.pop();
+        myLinkedList.poplast();
+        boolean result = myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myFirstNode);
+        Assert.assertTrue(result);
+    }
+}
