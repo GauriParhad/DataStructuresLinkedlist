7755905b72b906f0022b420372fbc31e194d06c6
Gaurip10 <thegauriparhad@gmail.com>
2021-08-20 22:02:43 +0530

    Added: Deleting first element

--- src/main/java/com/javapractice/MyNodeTest.java
+++ src/main/java/com/javapractice/MyNodeTest.java
@@ -1,8 +1,9 @@
 
 package com.javapractice;
-        import org.junit.Test;
-        import org.junit.Assert;
-@Tets
+import org.junit.Test;
+import org.junit.Assert;
+
+@Test
 public class MyLinkedList {
     public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
         MyNode<Integer> myFirstNode = new MyNODE<>(56);
@@ -14,6 +15,23 @@ public class MyLinkedList {
         myLinkedList.add(myThirdNode);
         boolean result = myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myFirstNode);
 
+        Assert.assertTrue(result);
+    }
+}
+@Test
+public class myLinkedList {
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
+        MyNode<Integer> myFirstNode = new MyNODE<>(56);
+        MyNode<Integer> mySecondNode = new MyNODE<>(30);
+        MyNode<Integer> myThirdNode = new MyNODE<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.insert(myFirstNode,mySecondNode);
+        myLinkedList.pop();
+        boolean result = myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myFirstNode);
         Assert.assertTrue(result);
     }
 }
