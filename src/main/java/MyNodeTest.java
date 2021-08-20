2080484c5be97d8defa7633aaa83f09b13a5e8c3
Gaurip10 <thegauriparhad@gmail.com>
2021-08-20 16:31:40 +0530

    Added: Ability to create Linked List by adding 30 and 56 to 70

--- src/main/java/MyNodeTest.java
+++ src/main/java/MyNodeTest.java
@@ -2,15 +2,18 @@ package com.javapractice;
 import org.junit.Test;
 import org.junit.Assert;
 
-public class MyNodeTest {
+public class myLinkedList {
     @Test
     public void given3NumbersWhenLinkedShouldPassedLinkedListTest(){
-        MyNode<Integer>myFirstNode=new MyNODE<>(key:56);
+        MyNode<Integer>myFirstNode=new MyNODE<>(key:70);
         MyNode<Integer>mySecondNode=new MyNODE<>(key:30);
-        MyNode<Integer>myThirdNode=new MyNODE<>(key:70);
-        myFirstNode.setNext(mySecondNode);
-        mySecondNode.setNext(myThirdNode);
-        boolean result=myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+        MyNode<Integer>myThirdNode=new MyNODE<>(key:56);
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+
+        boolean result=myLinkedList.head.equals(MyThirdNode) && myLinkedList.head.getNext().equals(MySecondNode) &&
+                myLinkedList.head.equals(MyfirstNode);
 
         Assert.assertTrue(result);
 
